b3e469bdbea0817065496c4e52e469d7a80e617c
Arun T K <arun.kalikeri@softappsit.com>
2018-01-21 16:07:21 +0530

    removed unused code

--- odoo.sh
+++ odoo.sh
@@ -7,7 +7,7 @@ header "Installing Odoo Runint Script... "
 ## Install Odoo runit service.
 run mkdir /etc/service/odoo
 run cp /pd_build/runit/odoo /etc/service/odoo/run
-
+run touch /etc/service/odoo/down
 header "Installing Odoo..."
 
 ## Install Odoo.
