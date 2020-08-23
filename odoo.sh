db55c4348ba8d6ca891e187ff74c4e7711c54773
Ubuntu <ubuntu@ip-172-31-27-42.ap-south-1.compute.internal>
2020-08-23 17:40:44 +0000

    removed multiple run

--- odoo.sh
+++ odoo.sh
@@ -2,23 +2,23 @@
 set -e
 source /pd_build/buildconfig
 
-header "Installing Odoo Runint Script... "
-
-## Install Odoo runit service.
-run mkdir -p /etc/service/odoo
-run cp /pd_build/runit/odoo /etc/service/odoo/run
-run touch /etc/service/odoo/down
 header "Installing Odoo..."
 
 ## Install Odoo.
 run sudo -i -u odoo
-curl -Lf -o /opt/odoo/odoo.tar.gz http://nightly.odoo.com/11.0/nightly/src/odoo_11.0.20171201.tar.gz
-run mkdir -p /opt/odoo/{bin,etc,sources/odoo/addons,additional_addons,data,scrape} && \
+curl -Lf -o /opt/odoo/odoo.tar.gz http://nightly.odoo.com/11.0/nightly/src/odoo_11.0.20171201.tar.gz \
+&& mkdir -p /opt/odoo/{bin,etc,sources/odoo/addons,additional_addons,data,scrape} && \
         cd /opt/odoo/sources/odoo && \
         tar -xvf /opt/odoo/odoo.tar.gz --strip 1 && \
 	cp -r -p /opt/odoo/sources/odoo/setup/odoo /opt/odoo/sources/odoo/openerp-server && \
-        rm /opt/odoo/odoo.tar.gz
-run mkdir -p /opt/odoo/var/{run,log,egg-cache,ftp}
+        rm /opt/odoo/odoo.tar.gz \
+&& mkdir -p /opt/odoo/var/{run,log,egg-cache,ftp} \
+&& cp /pd_build/sources/odoo.conf /opt/odoo/etc/odoo.conf && \
+chown -R odoo /opt/odoo
 
-run cp /pd_build/sources/odoo.conf /opt/odoo/etc/odoo.conf
-run chown -R odoo /opt/odoo
+#header "Installing Odoo Runint Script... "
+
+## Install Odoo runit service.
+#run mkdir -p /etc/service/odoo
+#run cp /pd_build/runit/odoo /etc/service/odoo/run
+#run touch /etc/service/odoo/down
